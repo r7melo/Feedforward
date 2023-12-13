3f11978605caa51711387225929aeb14cf8d951e
r7melo <romariomelo360@gmail.com>
2023-12-13 01:48:00 -0300

    finalizado, meu primeiro perceptron

--- Perceptron.py
+++ Perceptron.py
@@ -21,12 +21,15 @@ class Perceptron:
     def output(self) -> int:
         products = [ self.x[i]*self.w[i] for i in range(len(self.x)) ]
         S = sum(products+[self.bias])
-        return 1 if S >= 0 else 0
+        return 1 if expit(S) >= 0.5 else 0
     
     def correction(self, target:float, prediction:float) -> None:
         erro = target - prediction
         w = [(peso + LEARNING_RATE * erro * target) for peso in self.w]
         self.w = w
+
+        bias = self.bias + LEARNING_RATE * erro * target
+        self.bias
 
 
 if __name__=="__main__":
@@ -45,24 +48,27 @@ if __name__=="__main__":
         [0,0,0]
     ]
 
-    perceptron = Perceptron()
-    perceptron.input([1,1])
+    score = 0
+    while score < 4:
+        score = 0
 
-    y = perceptron.output()
+        perceptron = Perceptron()
 
-    perceptron.correction(1, y)
+        for epoca in range(100000):
+            for amostra in dataEND:
+                data = [amostra[0], amostra[1]]
+                perceptron.input(data)
+                y = perceptron.output()
+                perceptron.correction(amostra[2], y)
 
-    for epoca in range(1000):
-        for amostra in dataEND:
-            data = [amostra[0], amostra[1]]
-            perceptron.input(data)
+        
+        for data in dataEND:
+            perceptron.input([data[0], data[1]])
             y = perceptron.output()
-            perceptron.correction(amostra[2], y)
+            print(f"data={data} y={y}")
 
-    
-    for data in dataEND:
-        perceptron.input([data[0], data[1]])
-        y = perceptron.output()
-        print(f"data={data} y={y}")
+            if data[2] == y: score+=1
+        
+        print("")
 
    
